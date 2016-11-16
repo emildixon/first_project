e49652ddd39e08479e7b34feb29a54405a0ca70a
Emil Dixon <emildixon@yahoo.com>
2016-11-16 08:47:07 -0800

    5.2 added search feature and form styling

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,11 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+  get 'static_pages/landing_page'
+
   resources :orders, only: [:index, :show, :create, :destroy]
 
-root 'static_pages#about'
+root 'static_pages#landing_page'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
