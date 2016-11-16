db35b7ea9c6cd8c4db25eebf37cf6232c377a460
Emil Dixon <emildixon@yahoo.com>
2016-11-16 11:16:47 -0800

    5.3 added working contact form with Sendgrid

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,6 +7,16 @@ class StaticPagesController < ApplicationController
     @products = Product.limit(3)
   end
 
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+      :to => 'emildixon@yahoo.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+  end
+
 end
 
 
