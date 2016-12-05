610cf9fd029a58c41062635b35f62ff1e2ff7ad6
Emil Dixon <emildixon@yahoo.com>
2016-12-05 11:31:10 -0800

    5.8 Added Start Rating System

---
+++ app/assets/javascripts/site.js
@@ -0,0 +1,7 @@
+$(document).on('turbolinks:load', function (){
+  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+  $('.rated').raty( { path: '/assets', readOnly: true, score: function() {
+    return $(this).attr('data-score');
+    }
+  });
+});
