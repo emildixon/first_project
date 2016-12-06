a7dc8212c85ee70bebbd196818af60516948e9a0
Emil Dixon <emildixon@yahoo.com>
2016-12-06 09:01:05 -0800

    5.9 Validation & Pagination

--- Gemfile
+++ Gemfile
@@ -57,6 +57,8 @@ gem 'devise'
 
 gem 'cancancan', '~> 1.10'
 
+gem 'will_paginate', '~> 3.1.1'
+
 
 
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -9,4 +9,6 @@ class Product < ApplicationRecord
   def average_rating
     comments.average(:rating).to_f
   end
+
+  validates :name, presence: true
 end
