6ce98bd875ec8387d5af9dc2b3a53a54968f92e3
Emil Dixon <emildixon@yahoo.com>
2017-01-05 14:47:51 -0800

    6.6 Payments

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,34 @@
+class PaymentsController < ApplicationController
+  def create
+    #Determine product ID by reading hidden field in stripe checkout button partial
+    @product = Product.find(params[:product_id])
+    @user = current_user
+
+    token = params[:stripeToken]
+    # Create the charge on Stripe's servers - this will charge the user's card
+    begin
+      charge = Stripe::Charge.create(
+        :amount => @product.price.to_i*100, # amount in cents, again
+        :currency => "usd",
+        :source => token,
+        :description => params[:stripeEmail]
+      )
+    end
+
+    if charge.paid
+      Order.create!(
+        product_id: @product.id,
+        user_id: @user.id,
+        total: @product.price)
+      redirect_to product_path(@product)
+    end
+
+    rescue Stripe::CardError => e
+      # The card has been declined
+      body = e.json_body
+      err = body[:error]
+      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+    end
+
+end
+
