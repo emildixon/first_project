4137b6cc3de2f3110bf5b80b5ee8b83cf1825148
Emil Dixon <emildixon@yahoo.com>
2017-01-15 11:08:31 -0800

    6.9 added elevateZoom

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,7 +1,11 @@
 $(document).on('turbolinks:load', function (){
+  // Initialize Raty
   $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
   $('.rated').raty( { path: '/assets', readOnly: true, score: function() {
     return $(this).attr('data-score');
     }
   });
+
+  //Initialize elevateZoom
+  $('.img-zoom').elevateZoom();
 });
